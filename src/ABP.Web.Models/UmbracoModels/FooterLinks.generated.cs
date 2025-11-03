aff8546c42600d862bb201a811622e66f7621d52
Mujeeb/Deepak <mujeeb.rahman@espire.com>
2025-11-03 22:26:55 +0530

    Footer implementation

--- src/ABP.Web.Models/UmbracoModels/FooterLinks.generated.cs
+++ src/ABP.Web.Models/UmbracoModels/FooterLinks.generated.cs
@@ -18,14 +18,14 @@ using Umbraco.Extensions;
 
 namespace ABP.Web.Models.UmbracoModels
 {
-	/// <summary>Header Bottom Menu Item</summary>
-	[PublishedModel("headerBottomMenuItem")]
-	public partial class HeaderBottomMenuItem : PublishedElementModel
+	/// <summary>Footer Links</summary>
+	[PublishedModel("footerLinks")]
+	public partial class FooterLinks : PublishedElementModel
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		public new const string ModelTypeAlias = "headerBottomMenuItem";
+		public new const string ModelTypeAlias = "footerLinks";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
@@ -34,35 +34,20 @@ namespace ABP.Web.Models.UmbracoModels
 			=> PublishedModelUtility.GetModelContentType(contentTypeCache, ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
 		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedContentTypeCache contentTypeCache, Expression<Func<HeaderBottomMenuItem, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedContentTypeCache contentTypeCache, Expression<Func<FooterLinks, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(contentTypeCache), selector);
 #pragma warning restore 0109
 
 		private IPublishedValueFallback _publishedValueFallback;
 
 		// ctor
-		public HeaderBottomMenuItem(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
+		public FooterLinks(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
 			: base(content, publishedValueFallback)
 		{
 			_publishedValueFallback = publishedValueFallback;
 		}
 
 		// properties
-
-		///<summary>
-		/// Is a Button
-		///</summary>
-		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		[ImplementPropertyType("isAButton")]
-		public virtual bool IsAbutton => this.Value<bool>(_publishedValueFallback, "isAButton");
-
-		///<summary>
-		/// Mega Menu Items
-		///</summary>
-		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("megaMenuItems")]
-		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel MegaMenuItems => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "megaMenuItems");
 
 		///<summary>
 		/// Title
